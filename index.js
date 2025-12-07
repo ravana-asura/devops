04467ebf38bc866c6443500cdf65c229c6445e1a
Abhisek Gupta <rasura833@gmail.com>
2025-12-07 19:55:33 +0545

    the project is setup for building

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('DevOps module loaded');
+console.log("I am learning CI/CD Github Actions");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...");
+setInterval(() => {
+    console.log("Running tests for 3 second...");
+}, 3000);
+console.log("Tests setup complete.");
