cfa142107be6a09493a73a1599c426e2a9a9ea45
Abhisek Gupta <rasura833@gmail.com>
2025-12-08 18:50:13 +0545

    the loop build solved

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("Starting tests...");
-setInterval(() => {
-    console.log("Running tests for 3 second...");
+setTimeout(() => {
+  console.log("Running tests for 3 second...");
 }, 3000);
 console.log("Tests setup complete.");
